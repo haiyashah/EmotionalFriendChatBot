33d054ad97ba5f9d3b88c4ff1c388d6a568051a0
Haiya Shah <95308896+haiyashah@users.noreply.github.com>
2024-04-10 19:41:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,20 +17,6 @@ function processUserInputWithDelay() {
   const answer = userInput.value;
   generateChatBubble(`User: ${answer}`, true);
 
-  // Delayed response
-  setTimeout(() => {
-    if (answer.toLowerCase().includes("great")) {
-      generateChatBubble("Bot: Who all did you talk to?", false);
-    } else if (answer.toLowerCase().includes("friends and family")) {
-      generateChatBubble("Bot: Any good experience?", false);
-    } else if (answer.toLowerCase().includes("horrible")) {
-      generateChatBubble("Bot: I'm very sorry. Your mother was just afraid about you. I'm sure you had worked hard. Now cheer up, it was just an exam. Well, there is always something good in a bad day. Tell me, did you help anyone today?", false);
-    } else if (answer.toLowerCase().includes("yes")) {
-      generateChatBubble("Bot: That is such a sweet gesture, you are a great human being! These interpersonal skills will take you a long way in life!", false);
-    } else {
-      generateChatBubble("Bot: Tell me about your day?", false);
-    }
-
     userInput.value = "";
   }, 2000); // 3 seconds delay
 }
